f22ea996937d5696fda1d4ecae1a475313370349
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 19:10:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,16 +4,14 @@ class String
 
   def sentence?
     self[-1] == "." ? true : false
-
-
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
